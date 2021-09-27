88abc91a54cfd2dbc5b7fb2b09da3bc9b51c9c07
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2021-09-27 10:24:22 -0300

    Create computed.vue

---
+++ computed.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+   <input
+   type="checkbox"
+   v-model="nomeDocomputed"
+    />
+  </div>
+
+</template>
+<script>
+  export default {
+   computed:{
+        testeInput(){
+            
+            return true;
+        }
+    }
+  }
+
+</script>
